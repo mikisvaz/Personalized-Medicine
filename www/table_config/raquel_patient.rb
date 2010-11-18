1c0968afc4328ed899540e6edf62573911298257
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2010-11-18 01:23:35 +0100

    Two new columns for Raquel_Patient

--- www/table_config/raquel_patient.rb
+++ www/table_config/raquel_patient.rb
@@ -7,7 +7,7 @@ field "Top Lost Genes" do
     v = TSV.zip_fields(values)
     list = []
     v.each{|vv| if vv["top5_loss"] == "1";then list << vv.first end}
-    list.sort
+    list.sort * ", "
   end
 
   sort_by do |key,values|
@@ -18,12 +18,19 @@ field "Top Lost Genes" do
   end
 end
 
+field "Number of Gene Sig. Lost (0.05)" do
+  show do |key,values|
+    v = TSV.zip_fields(values)
+    v.select{|vv| vv["type"] == "Loss" && vv["probability"].to_f.abs > 0.95}.length
+  end
+end
+
 field "Top Gain Genes" do
   show do |key,values|
     v = TSV.zip_fields(values)
     list = []
     v.each{|vv| if vv["top5_gain"] == "1";then list << vv.first end}
-    list.sort
+    list.sort * ", "
   end
 
   sort_by do |key,values|
@@ -31,6 +38,13 @@ field "Top Gain Genes" do
     list = []
     v.each{|vv| if vv["top5_gain"] == "1";then list << vv.first end}
     list.sort * " "
+  end
+end
+
+field "Number of Gene Sig. Gain (0.05)" do
+  show do |key,values|
+    v = TSV.zip_fields(values)
+    v.select{|vv| vv["type"] == "Gain" && vv["probability"].to_f.abs > 0.95}.length
   end
 end
 
